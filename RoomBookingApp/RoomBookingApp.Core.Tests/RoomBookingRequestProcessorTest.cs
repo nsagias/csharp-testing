a9aa46a6a42b8c02bbbeaf1b8584fa5a988effbd
Nick <nsagias@gmail.com>
2021-12-30 20:35:39 -0500

    Create bookRequest

--- RoomBookingApp/RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp/RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -7,6 +7,13 @@ namespace RoomBookingApp.Core {
         [Fact]
         public void Should_Return_Room_Booking_Request() {
 
+            var bookingRequest = new RoomBookingReques() {
+                FullName = "Test name",
+                Email = "test@example.com",
+                Date = new DateTime(2021, 12, 30);
+            }
+
+            var processor = new RoomBookingRequestProcessor();
         }
     }
 }
