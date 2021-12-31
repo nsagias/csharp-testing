ca5ccf36273495af9a1696bd5e68f0297730fec7
Nick <nsagias@gmail.com>
2021-12-30 20:36:51 -0500

    fix syntax

--- RoomBookingApp/RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp/RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -7,11 +7,12 @@ namespace RoomBookingApp.Core {
         [Fact]
         public void Should_Return_Room_Booking_Request() {
 
-            var bookingRequest = new RoomBookingReques() {
+            var bookingRequest = new RoomBookingReques()
+            {
                 FullName = "Test name",
                 Email = "test@example.com",
-                Date = new DateTime(2021, 12, 30);
-            }
+                Date = new DateTime(2021, 12, 30)
+            };
 
             var processor = new RoomBookingRequestProcessor();
         }
